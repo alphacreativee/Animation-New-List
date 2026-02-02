62ccd94f2cf872b6f999633cf46098f4c1d7eaa8
TAOX3 <tngochai04@gmail.com>
2026-02-02 15:38:38 +0700

    add link demo

---
+++ assets/js/index/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const lenis = new Lenis();
+  lenis.on("scroll", ScrollTrigger.update);
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+  gsap.ticker.lagSmoothing(0);
+});
